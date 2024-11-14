f3cd0c78b1994f382d100fcc5538443944182cea
Avinash Nair <avinair108@gmail.com>
2024-11-14 15:43:24 -0500

    pruning dataset

--- src/dataset.py
+++ src/dataset.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("hf://datasets/google/frames-benchmark/test.tsv", sep="\t")
 
-print(df.head(5))
+# filter for 3 wiki links or less
+df_filtered = df[df["wiki_links"].str.count(",") <= 2]
+
+# print(df_filtered.head(2))
+
+# Keep only the 'Prompt' and 'wiki_links' columns
+df_filtered = df_filtered.loc[:, ["Prompt", "wiki_links"]]
+
+print(df_filtered.shape)
