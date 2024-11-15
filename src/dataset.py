e5f02fb4250c82eaf1c815b76486f6a7553e96fc
Avinash Nair <avinair108@gmail.com>
2024-11-15 18:45:08 -0500

    preparing dataset for easier processing

--- src/dataset.py
+++ src/dataset.py
@@ -8,6 +8,10 @@ df_filtered = df[df["wiki_links"].str.count(",") <= 2]
 # print(df_filtered.head(2))
 
 # Keep only the 'Prompt' and 'wiki_links' columns
-df_filtered = df_filtered.loc[:, ["Prompt", "wiki_links"]]
+df_filtered = df_filtered.loc[:, ["Prompt", "reasoning_types", "wiki_links"]]
 
+# keep track of the number of wiki links as ground truth
+df_filtered["query_count"] = df_filtered["wiki_links"].str.count(",") + 1
+
+print(df_filtered.head(2))
 print(df_filtered.shape)
