69f7bccba6d6f1ee3f8077f872e54bd967e3ae5b
Avinash Nair <avinair108@gmail.com>
2024-11-15 19:42:49 -0500

    extracting keywords to another column

--- src/dataset.py
+++ src/dataset.py
@@ -13,5 +13,16 @@ df_filtered = df_filtered.loc[:, ["Prompt", "reasoning_types", "wiki_links"]]
 # keep track of the number of wiki links as ground truth
 df_filtered["query_count"] = df_filtered["wiki_links"].str.count(",") + 1
 
+
+def extract_keywords(links):
+    keywords = []
+    for link in links.split(","):
+        if "https://en.wikipedia.org/wiki/" in link:
+            keywords.append(link.split("https://en.wikipedia.org/wiki/")[1])
+    return ",".join(keywords)
+
+
+df_filtered["keywords"] = df_filtered["wiki_links"].apply(extract_keywords)
+
 print(df_filtered.head(2))
 print(df_filtered.shape)
