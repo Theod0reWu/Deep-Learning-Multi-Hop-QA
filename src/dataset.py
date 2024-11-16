890b7f36607d341ea8a29df6363541a23eb9fe58
Theod0reWu <43049406+Theod0reWu@users.noreply.github.com>
2024-11-16 13:41:20 -0500

    edited to make get df a function

--- src/dataset.py
+++ src/dataset.py
@@ -1,28 +1,26 @@
 import pandas as pd
 
-df = pd.read_csv("hf://datasets/google/frames-benchmark/test.tsv", sep="\t")
+    def extract_keywords(links):
+        keywords = []
+        for link in links.split(","):
+            if "https://en.wikipedia.org/wiki/" in link:
+                keywords.append(link.split("https://en.wikipedia.org/wiki/")[1])
+        return ",".join(keywords)
 
-# filter for 3 wiki links or less
-df_filtered = df[df["wiki_links"].str.count(",") <= 2]
+def get_frames_dataset():
+    df = pd.read_csv("hf://datasets/google/frames-benchmark/test.tsv", sep="\t")
 
-# print(df_filtered.head(2))
+    # filter for 3 wiki links or less
+    df_filtered = df[df["wiki_links"].str.count(",") <= 2]
 
-# Keep only the 'Prompt' and 'wiki_links' columns
-df_filtered = df_filtered.loc[:, ["Prompt", "reasoning_types", "wiki_links"]]
+    # Keep only the 'Prompt' and 'wiki_links' columns
+    df_filtered = df_filtered.loc[:, ["Prompt", "reasoning_types", "wiki_links"]]
 
-# keep track of the number of wiki links as ground truth
-df_filtered["query_count"] = df_filtered["wiki_links"].str.count(",") + 1
-
-
-def extract_keywords(links):
-    keywords = []
-    for link in links.split(","):
-        if "https://en.wikipedia.org/wiki/" in link:
-            keywords.append(link.split("https://en.wikipedia.org/wiki/")[1])
-    return ",".join(keywords)
+    # keep track of the number of wiki links as ground truth
+    df_filtered["query_count"] = df_filtered["wiki_links"].str.count(",") + 1
 
 
-df_filtered["keywords"] = df_filtered["wiki_links"].apply(extract_keywords)
+    df_filtered["keywords"] = df_filtered["wiki_links"].apply(extract_keywords)
 
-print(df_filtered.head(2))
-print(df_filtered.shape)
+    return df_filtered
+
